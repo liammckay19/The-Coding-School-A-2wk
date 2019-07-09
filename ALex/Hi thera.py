76666e2aa0efa5870f3c6e64169956624acaa3f6
magicyetifire <magicyetifire@outlook.com>
2019-07-09 14:04:01 -0500

    Initial commit

---
+++ ALex/Hi thera.py
@@ -0,0 +1,11 @@
+#File for Hi ther
+
+print("I cant wait to learn how to create text based games that allow for different user input")
+print(str(5)+"days")
+print("     $     $")
+print("     $     $")
+print("     $     $")
+print("     $     $")
+print(" $               $ ")
+print("   $           $")
+print("      $$$$$$$")
