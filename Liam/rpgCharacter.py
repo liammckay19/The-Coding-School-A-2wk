a298cfdfd9b8597263203c37b123bb3b63ce8b54
Liam McKay <ltmckay@ucsc.edu>
2019-07-16 13:08:00 -0700

    rpg code reformatted

--- Liam/rpgCharacter.py
+++ Liam/rpgCharacter.py
@@ -1,11 +1,13 @@
 import random
+
 # TODO: spells dictionary with damage/healing/item enchanting modifier
 
 weaponsDictionary = { "hands": {
-    "default":0,
-    "enchanted":1
-}, "sword": {"default":4, "enchanted":6} }  # extra damage
-hidingSpots = { "boulder":4, "tree":3, "small bush":2, "blade of grass":1}  # value is hiding spot effectiveness
+    "default": 0,
+    "enchanted": 1
+}, "sword": { "default": 4, "enchanted": 6 } }  # extra damage
+hidingSpots = { "boulder": 4, "tree": 3, "small bush": 2, "blade of grass": 1 }  # value is hiding spot effectiveness
+
 
 class Player(object):
     def __init__(self, health=100, armorArgument=0, level=0, weapon='hands', stealth=0):
@@ -17,14 +19,14 @@ class Player(object):
         self.level = level
         self.weaponIsEnchanted = False
         self.stealth = stealth
-        self.spellList = ["Heal","Enchantment","Damage","Boost"]
+        self.spellList = ["Heal", "Enchantment", "Damage", "Boost"]
     
     # attack (special attack, with a sword)
     # kick (physical attack)
     # run (away?)
     # hide
     # spell (can do more than just attack the enemy)
-
+    
     # read-only methods
     # we are getting an attribute (read-only)
     def getHealth(self):
@@ -46,16 +48,16 @@ class Player(object):
     def getLevel(self):
         # TODO: add doc-string
         return self.level
-
+    
     def attack(self, otherEntity):
         # TODO: add doc-string
         if self.weaponIsEnchanted:
             attackDamage = (1 + weaponsDictionary[self.getWeapon()]['enchanted']) * self.damageModifier
         else:
             attackDamage = (1 + weaponsDictionary[self.getWeapon()]['default']) * self.damageModifier
-
+        
         otherEntity.health -= attackDamage
-
+        
         return attackDamage
     
     def kick(self, otherEntity):
@@ -66,7 +68,7 @@ class Player(object):
     
     def run(self):
         # TODO: add random events
-
+        
         randomGeneratorInteger = random.randint(0, 3)
         # TODO: make if logic and print running actions
         if randomGeneratorInteger == 0:
@@ -78,7 +80,7 @@ class Player(object):
             print("You trip while running away, and fall on a rock. -10 HP")
             self.health -= 10
         return
-
+    
     def hide(self):
         # TODO: add doc-string
         # Possible hiding spots (random hiding number): (the lower a random number, the worse the spot)
@@ -90,12 +92,13 @@ class Player(object):
         print("You hid behind a {0}.".format(hidingSpots[randomHidingNumber]))
         # possibilityforamiss = 20 * randomHidingNumber *
         pass
+    
     # ogre who is about to attack
-        # player who is hiding
-        # if there is a random number greater than possibilityforamiss, then the ogre misses
-        # the higher the number of possibilityforamiss, then the higher the chance of the ogre missing.
-        # TODO: program this algorithm for hiding
-        
+    # player who is hiding
+    # if there is a random number greater than possibilityforamiss, then the ogre misses
+    # the higher the number of possibilityforamiss, then the higher the chance of the ogre missing.
+    # TODO: program this algorithm for hiding
+    
     def spell(self):
         # TODO: add doc-string
         # Come up with names for these spells to print to the player, save them in spellList attribute
@@ -105,7 +108,7 @@ class Player(object):
         # enchant weapon (boost damage for your weapon)
         # TODO: enchant weapon code goes here:
         pass
-        
+
 
 class Enemy(object):
     # these methods are very similar to the Player
